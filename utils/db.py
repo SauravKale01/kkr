53c925bca65595f6857f492e081e2fee7ae0555b
SauravKale01 <139038240+SauravKale01@users.noreply.github.com>
2023-08-01 00:15:49 +0530

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,7 +1,7 @@
 from pymongo.mongo_client import MongoClient
 from datetime import date
 
-uri = "MONGO_DB_URL"
+uri = "mongodb+srv://vortex:yNNrzMsR0BAiI4iY@cluster0.8sizo.mongodb.net/?retryWrites=true&w=majority"
 mongo = MongoClient(uri).Rankings
 chatdb = mongo.chat
 
